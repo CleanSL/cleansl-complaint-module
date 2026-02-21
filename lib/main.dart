34b9536ebac8048d0aaeb9c9a0dc99d1c111ac79
Nafhath <mohamednafhath1@gmail.com>
2026-02-22 01:05:59 +0530

    Some errors fixed in main.dart and pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // theme: AppTheme.lightTheme
+      theme: AppTheme.lightTheme,
       home: const ComplaintScreen(),
     );
   }
